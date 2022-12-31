2d50f1a9079e214be2114018ce45ac8e729389ec
AbdulrahmanAlosaimi <osaimi-a@hotmail.com>
2022-12-31 17:31:32 +0300

    Changed groups table name, added column for repo links

--- migrations/sqls/20221228202355-groups-table-up.sql
+++ migrations/sqls/20221228202355-groups-table-up.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS groups (
+CREATE TABLE IF NOT EXISTS projects (
    id VARCHAR(50) PRIMARY KEY NOT NULL,
    project_title VARCHAR(50),
    project_desc TEXT,
+   repo_link TEXT,
    supervisor_id INTEGER REFERENCES supervisors(uid)
 );
