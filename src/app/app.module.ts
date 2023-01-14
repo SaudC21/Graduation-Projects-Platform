4bb45cc676d0363604d11a15ecfd8c13d2f349e8
AbdulrahmanAlosaimi <osaimi-a@hotmail.com>
2023-01-14 21:47:37 +0300

    Login page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from './../environments/environment';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
