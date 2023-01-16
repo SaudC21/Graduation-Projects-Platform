823119d5f7f9a91ab0c0ea24b6e3d215fe220608
SaudC21 <saudalslimany@gmail.com>
2023-01-16 21:28:24 +0300

    creation of main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginPageComponent,
   },
-  { path: 'register', component: RegisterPageComponent },
+  {
+    path: 'register',
+    component: RegisterPageComponent
+  },
+  {
+    path: 'dashboard',
+    component: MainPageComponent,
+    children: [{
+      path: '',
+      component: DashboardComponent,
+      outlet: 'test'
+    }, {
+      path: 'test',
+      component: LoginPageComponent,
+      outlet: 'test',
+    }]
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  @ViewChild('button') elRef: ElementRef | any;
+
+  constructor(private router: Router){
+
+  }
+
+  routeLogin(event: any){
+    console.log(event)
+    this.router.navigate(['/dashboard']);
+  }
+
+  routeRegister(event: any){
+    console.log(event)
+    this.router.navigate(['/register']);
+  }
 
 }
