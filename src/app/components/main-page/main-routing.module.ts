d9f803c7df5be21b65fa8aa3d89bfb7714faa1a7
AbdulrahmanAlosaimi <osaimi-a@hotmail.com>
2023-01-26 00:25:09 +0300

    Fixed routing functionality

---
+++ src/app/components/main-page/main-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { LoginPageComponent } from '../login-page/login-page.component';
+import { MainPageComponent } from './main-page.component';
+
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainPageComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'test',
+        component: LoginPageComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class MainRoutingModule {}
