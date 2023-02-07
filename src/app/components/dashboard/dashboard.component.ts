87a633305c1b3788851202adbb97fe427947cdd3
SaudC21 <saudalslimany@gmail.com>
2023-02-08 02:27:07 +0300

    Completed form validation and confirmation page

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { PrimeIcons } from 'primeng/api';
-import { TimelineElement } from '../horizontal-timeline/timeline-element';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,12 +7,10 @@ import { TimelineElement } from '../horizontal-timeline/timeline-element';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  timeline: TimelineElement[] = [];
   constructor(private router: Router) {
 
   }
   ngOnInit() {
-    this.timeline = [{ caption: 'saud', date: new Date(), title: 'saud', content: 'saud' }, { caption: 'faisal', date: new Date(), title: 'faisal', content: 'faisal' }, { caption: 'abdulrahman', date: new Date(), title: 'abdulrahman', content: 'abdulrahman' }];
   }
   routeTest() {
     this.router.navigate(['../test']);
